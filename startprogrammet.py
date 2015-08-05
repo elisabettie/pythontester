82943d1a18446d74363395018ace27d766d0de02
elisabettan <ElisabetJohansson@Elisabets-MacBook-Pro.local>
2015-08-05 14:23:52 +0200

    uppgift 7 klar

--- startprogrammet.py
+++ startprogrammet.py
@@ -1,5 +1,5 @@
 #coding: utf-8
-#--- uppgift 6---
+#--- uppgift 7---
 class Product(object):
 	price = 0
 	count = 0
@@ -13,11 +13,11 @@ class Product(object):
 	def price_with_tax(self):
 		return self.price * self.count * self.tax
 
-robot = Product(price=900, count=2, tax=1.25)
+products = [Product(price=900, count=2, tax=1.25), Product(price=100, count=1, tax=1.06)]
 
-book = Product(price=100, count=1, tax=1.06)
+total_price = products[0].price_with_tax() + products[1].price_with_tax()
 
-print robot.price_with_tax() + book.price_with_tax()
+print total_price
 
 #--- uppgift 6---
 #class Product(object):
