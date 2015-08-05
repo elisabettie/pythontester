7908d65b8b1e3e9f63d7d624be448281b45854e6
elisabettan <ElisabetJohansson@Elisabets-MacBook-Pro.local>
2015-08-05 11:55:51 +0200

    gjort fram till uppgift 5

--- startprogrammet.py
+++ startprogrammet.py
@@ -1,1 +1,64 @@
-print 900*2*1.25+100*1.06 
+#coding: utf-8
+#---uppgift 5---
+#class Product(object):
+#	price = 0
+#	count = 0
+#	tax = 1
+#
+#	def price_with_tax(self):
+#		return self.price * self.count * self.tax
+#
+#robot = Product()
+#robot.price = 900
+#robot.count = 2
+#robot.tax = 1.25
+#
+#book = Product()
+#book.price = 900
+#book.count = 1
+#book.tax = 1.06
+#
+#print robot.price_with_tax() + book.price_with_tax()
+
+#--- uppgift 4 ---
+#class Product(object):
+#	price = 0
+#	count = 0
+#	tax = 1
+
+#robot = Product()
+#robot.price = 900
+#robot.count = 2
+#robot.tax = 1.25
+
+#book = Product()
+#book.price = 900
+#book.count = 1
+#book.tax = 1.06
+
+#print robot.price * robot.count * robot.tax + book.price * book.count * book.tax
+
+
+#--- uppgift 3 ---
+#robot = {"price":900, "count":2, "tax":1.25}
+#book = {"price":100, "count":1, "tax":1.06}
+
+
+#print robot["price"] * robot["count"] * robot["tax"] + book["price"] * book["count"] * book["tax"]
+
+#---Här är en lista---
+#customer_id[10,20,30]
+#print customer_id[0]
+
+#customer_namebase["bob","anna","lisa"]
+#customer_namebase["anna"]
+
+#---uppgift 2---
+#robot_price = 900
+#robot_count = 2
+#robot_tax = 1.25
+
+#book_price = 100
+#book_count = 1
+#book_tax = 1.06
+#print robot_price * robot_count * robot_tax + book_price * book_count * book_tax 
